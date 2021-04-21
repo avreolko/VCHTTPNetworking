2a2ba2c60fae2f11e8e93e1aa0ffeae9cdef5e94
Валентин Черепянко <vacherepyanko@sberbank.ru>
2021-04-21 19:22:04 +0700

    added missing http methods

--- Sources/RequestBuilder.swift
+++ Sources/RequestBuilder.swift
@@ -29,9 +29,14 @@ import Foundation
 public enum HTTPMethod: String
 {
     case get = "GET"
+    case head = "HEAD"
     case post = "POST"
-    case delete = "DELETE"
     case put = "PUT"
+    case delete = "DELETE"
+    case connect = "CONNECT"
+    case options = "OPTIONS"
+    case trace = "TRACE"
+    case PATCH = "PATCH"
 }
 
 public final class RequestBuilder {
